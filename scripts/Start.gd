dd2061ee13711e50b4f509f44ea8d44a26f543d8
Jenna <jennawing3@gmail.com>
2023-01-28 17:09:54 -0500

    add block shatter animation, start page and instructions

--- scripts/Start.gd
+++ scripts/Start.gd
@@ -2,13 +2,5 @@ extends Control
 
 
 func _on_Button_pressed():
-	$Tween.interpolate_property($Button, "rect_position:y", 64, 512, 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Tween.start()
-
-func _on_Tween_tween_all_completed():
-	# TODO: play dust animation
-	$Timer.start()
-
-
-func _on_Timer_timeout():
 	get_tree().change_scene("res://scenes/Game.tscn")
+	
